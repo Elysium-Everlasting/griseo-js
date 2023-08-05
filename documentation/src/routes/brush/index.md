2f898e7e53590a943677ea32035cefbc999f2e45
Aponia <brianmv@uci.edu>
2023-08-05 08:19:39 -0700

    refactor: clean up documentation project

--- documentation/src/routes/brush/index.md
+++ documentation/src/routes/brush/index.md
@@ -13,3 +13,21 @@ next:
 <div class="my-4 flex justify-center">
   <img src="/images/brush.png" height="250" />
 </div>
+
+```ts twoslash
+interface IdLabel {
+  id: number /* some fields */
+}
+interface NameLabel {
+  name: string /* other fields */
+}
+type NameOrId<T extends number | string> = T extends number ? IdLabel : NameLabel
+// This comment should not be included
+
+// ---cut---
+function createLabel<T extends number | string>(idOrName: T): NameOrId<T> {
+  throw 'unimplemented'
+}
+
+let a = createLabel('typescript')
+```
--- documentation/vuepress.config.ts
+++ documentation/vuepress.config.ts
@@ -3,7 +3,6 @@ import UnoCSS from 'unocss/vite'
 import { presetWind } from 'unocss/preset-wind'
 import { defineUserConfig, defaultTheme } from 'vuepress'
 import { markdownItShikiTwoslashSetup } from 'markdown-it-shiki-twoslash'
-import { tocPlugin } from '@vuepress/plugin-toc'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
@@ -31,13 +30,12 @@ const config: ReturnType<typeof defineUserConfig> = defineUserConfig({
     '@theme/HomeFooter.vue': './src/layout/HomeFooter.vue',
   },
 
-  async extendsMarkdown(_extendable) {
+  async extendsMarkdown(extendable) {
     const shiki = await markdownItShikiTwoslashSetup({
       theme: 'nord',
-      addTryButton: true,
     })
+    extendable.use
     shiki
-    // extendable.use(shiki)
   },
   theme: defaultTheme({
     repo: repository.url,
@@ -48,7 +46,6 @@ const config: ReturnType<typeof defineUserConfig> = defineUserConfig({
     registerComponentsPlugin({
       componentsDir: './src/components',
     }),
-    tocPlugin({}),
     backToTopPlugin(),
   ],
   bundler: viteBundler({
